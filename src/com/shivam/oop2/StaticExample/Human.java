7bba4983d49957fce48b23c746bb9708c08b2dee
SHIVAM-KUMAR-59 <jsrshivamx7@gmail.com>
2024-12-01 19:56:55 +0530

    Comments added and updated README.md

--- src/com/shivam/oop2/StaticExample/Human.java
+++ src/com/shivam/oop2/StaticExample/Human.java
@@ -1,18 +1,23 @@
 package com.shivam.oop2.StaticExample;
 
 public class Human {
-    int age;
-    String name;
-    int salary;
-    boolean married;
-    static long population;
+    // Instance variables (specific to each object)
+    int age;        // Age of the human
+    String name;    // Name of the human
+    int salary;     // Salary of the human
+    boolean married; // Marital status of the human
 
+    // Static variable (shared among all objects of the class)
+    static long population; // Total population of humans, shared across all instances
+
+    // Constructor to initialize a new Human object
     public Human(int age, String name, int salary, boolean married) {
-        this.age = age;
-        this.name = name;
-        this.salary = salary;
-        this.married = married;
+        this.age = age;          // Assigning age to the instance variable
+        this.name = name;        // Assigning name to the instance variable
+        this.salary = salary;    // Assigning salary to the instance variable
+        this.married = married;  // Assigning marital status to the instance variable
 
+        // Incrementing the static population variable for each new Human object created
         Human.population += 1;
     }
 }
--- src/com/shivam/oop2/StaticExample/Main.java
+++ src/com/shivam/oop2/StaticExample/Main.java
@@ -3,10 +3,42 @@ package com.shivam.oop2.StaticExample;
 public class Main {
     public static void main(String[] args) {
 
+        // Creating instances of the Human class with different attributes
         Human shivam = new Human(20, "Shivam", 120000, false);
         Human pihu = new Human(20, "Pihu", 130000, false);
+        Human mauli = new Human(20, "Mauli", 120000, true);
 
+        // Accessing the static member 'population' using different instances
+        // All instances will share the same value for static variable 'population'
         System.out.println(shivam.population);
         System.out.println(pihu.population);
+        System.out.println(mauli.population);
+
+        fun();
+
     }
+
+    // Static method
+    static void fun() {
+        // greeting();
+        // This will give an error because 'greeting()' is a non-static method,
+        // and non-static methods belong to an instance of the class, not the class itself.
+
+        /*
+        * You cannot access non-static stuff without referencing their instances in a static context,
+        * hence here we can use the 'greeting' method by referencing it by the 'Main' class
+        */
+        Main obj = new Main();
+        obj.greeting();
+    }
+
+    // Non-static method
+    void greeting() {
+        System.out.println("Hello World");
+
+        // Calling a static method from a non-static context
+        // This is allowed because static methods are class-level members and accessible globally.
+        fun();
+    }
+
 }
